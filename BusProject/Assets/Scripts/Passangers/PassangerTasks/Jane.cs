bb0d51d1da598cddf464a588162c78b399ab67e0
Cookiegithubby <126580170+Cookiegithubby@users.noreply.github.com>
2025-12-17 09:24:21 +0100

    Jane Task fix

--- BusProject/Assets/Scripts/Passangers/PassangerTasks/Jane.cs
+++ BusProject/Assets/Scripts/Passangers/PassangerTasks/Jane.cs
@@ -4,6 +4,7 @@ public class Jane : PassangerInteraction
 {
     [SerializeField] Transform headPhones;
     private Transform initialHeadphoneTransform;
+    [SerializeField] float headphoneDist;
 
     private void Start()
     {
@@ -12,7 +13,9 @@ public class Jane : PassangerInteraction
 
     protected override void TaskInteract()
     {
-        if (headPhones.position != initialHeadphoneTransform.position)
+        if (Mathf.Abs(headPhones.position.x - initialHeadphoneTransform.position.x) > headphoneDist ||
+            Mathf.Abs(headPhones.position.y - initialHeadphoneTransform.position.y) > headphoneDist ||
+            Mathf.Abs(headPhones.position.z - initialHeadphoneTransform.position.z) > headphoneDist)
             passanger.state = currentState.MoveToWindow;
 
         base.TaskInteract();
