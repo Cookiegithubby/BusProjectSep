ddecbf82d27cca884a303bcbaeb55210d61b65fc
Cookiegithubby <126580170+Cookiegithubby@users.noreply.github.com>
2025-12-17 15:07:21 +0100

    Dynamisk opdatering af score

--- BusProject/Assets/Scripts/Scoring/Showscore.cs
+++ BusProject/Assets/Scripts/Scoring/Showscore.cs
@@ -5,10 +5,18 @@ public class Showscore : MonoBehaviour
 {
     private ScoreManager scoreManager;
     private TextMeshProUGUI textMeshProUGUI;
+    [SerializeField] private bool updateOnFrame;
     void Start()
     {
         scoreManager = GameObject.FindGameObjectWithTag("Scoremanager").GetComponent<ScoreManager>();
         textMeshProUGUI = GetComponent<TextMeshProUGUI>();
+
+        textMeshProUGUI.text = "Score: " + scoreManager.getScore();
+    }
+
+    private void Update()
+    {
+        if (!updateOnFrame) return;
 
         textMeshProUGUI.text = "Score: " + scoreManager.getScore();
     }
