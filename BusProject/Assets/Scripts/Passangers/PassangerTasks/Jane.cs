61aa103cdb40ebbd79e2dda77c56654ed12fac78
Cookiegithubby <126580170+Cookiegithubby@users.noreply.github.com>
2025-12-17 11:53:42 +0100

    Jane final fix (Surelyyy)

--- BusProject/Assets/Scripts/Passangers/PassangerTasks/Jane.cs
+++ BusProject/Assets/Scripts/Passangers/PassangerTasks/Jane.cs
@@ -2,16 +2,5 @@ using UnityEngine;
 
 public class Jane : PassangerInteraction
 {
-    [SerializeField] float headphoneDist;
-    private bool headphonesMoved;
-
-    protected override void TaskInteract()
-    {
-        if (headphonesMoved && passanger.state == currentState.Task)
-            passanger.state = currentState.MoveToWindow;
-
-        base.TaskInteract();
-    }
-
-    public void HeadPhonesMoved() { headphonesMoved = true; }
+    public void HeadPhonesMoved() { passanger.state = currentState.MoveToWindow; PlayerInteraction(); }
 }
